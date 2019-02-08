ab744de7e6a1402b2d86f3bfedb7f4598a10fd69
= <=>
2019-02-08 15:05:07 +0530

    about link added in home page

--- example_8.php
+++ example_8.php
@@ -8,8 +8,16 @@
 <html>
 	<head>
 		<title>Title</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
+		<nav class="navbar">
+			<ul class="navbar-nav">
+				<li class="nav-item">About page</li>
+			</ul>
+		</nav>
 		<p>Deserts</p>
 		<ul>
 			<li>Cupcakes</li>
